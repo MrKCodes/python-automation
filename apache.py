de5124134d83893370f4e9207c441076dbdea7ca
Kartikeya Chauhan <kartikeyasingh76@gmail.com>
2020-07-01 15:01:52 +0530

    added nore modules like compile or install

--- apache.py
+++ apache.py
@@ -1,8 +1,7 @@
 import os
 import subprocess
 import tarfile
-
-
+import shutil
 
 # Dependencies installations
 
@@ -13,11 +12,68 @@ def installDependencies():
     subprocess.call(command2)
     print("PCRE and Open SSL  dependencies are installed")
 
-def downloadMedia():
-    # downloadinf media for apachae and apr utils
-    commandApache = "wget", "-c "
-    commandApr = "wget", "-c "
-    commandAprUtils = "wget","-c "
+def downloadMedia(apacheV, aprV, aprutilV):
+    # downloadinf media for apache and apr utils
+    commandApache = "wget", "-c http://www.apache.org/dist/httpd" + apacheV + ".tar.gz"
+    commandApr = "wget", "-c http://mirror.cogentco.com/pub/apache/apr/" + aprV +".tar.gz"
+    commandAprUtils = "wget","-c http://mirror.cogentco.com/pub/apache/apr/" + aprutilV + ".tar.gz"
 
     ##
     os.chdir("/usr/src")
+    subprocess.call(commandApache)
+    subprocess.call(commandApr)
+    subprocess.call(commandAprUtils)
+
+    # Apache package extraction
+    try:
+        print("Extracting Apache TAR")
+        tar_apache = tarfile.open(apacheV)
+        tar_apache.extractall() # specify which folder to extract to
+        tar_apache.close()
+    except:
+        print("Something went wrong")
+    
+
+    # Apr package extraction
+    try:
+        print("Extracting APR TAR")
+        tar_apr = tarfile(aprV)
+        tar_apr.extractall()
+        tar_apr.close()
+    except:
+        print("Something went wrong")
+
+    # Apr utils package extraction
+    try:
+        print("Extracting AprUtils TAR")
+        tar_aprutils = tarfile(aprutilV)
+        tar_aprutils.extractall()
+        tar_aprutils.close()
+    except:
+        print("Something went wrong")
+
+    print("Moving the apr and apr-utils to apache source")
+    destination = "/usr/src/" + apacheV
+    ## Moving apr and aprutils to use/src/httpd-2.4
+    shutil.move(aprV, destination)
+    shutil.move(aprutilV, destination)
+
+def compileSrc(apacheSrc):
+    # apacheSrc is the apache path as /usr/src/httpd2.4.25
+    os.chdir(apacheSrc)
+    try:
+        configure = "./configure", "--enable-so --enable-ssl --with-mpm=prefork --with-included-apr"
+        subprocess.call(configure)
+        subprocess.call("make")
+        subprocess.call("make", "install")
+    except:
+        print("Something went wrong")
+
+def postInstall():
+    # Will contain modules like:
+    ## enable ssl by editing httpd.conf
+    ## start apache
+    ## enable socache_shmcb
+    ## copy the keys and certs
+    ## 
+    print()
